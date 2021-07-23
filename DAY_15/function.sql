a4f55191fb991bae1227808595e0f8e631e4f075
Zzeongyx2 <39085743+Zzeongyx2@users.noreply.github.com>
2021-07-23 11:02:05 +0900

    Create function.sql

---
+++ DAY_15/function.sql
@@ -0,0 +1,158 @@
+--실습1
+--현재 시간 조회
+select now();
+
+--현재 시간 - 1일
+select now(), now()::date - '1 day' ::interval;
+
+--현재 타임존 조회
+show timezone;
+
+--시스템 일자
+select current_date, current_time, timeofday();
+
+select now(), current_timestamp, timestamp 'now';
+
+--날짜에서 연도 추출
+select date_part('year', timestamp '2020-07-30 20:38:40');
+select date_part('year', current_timestamp);
+
+select extract('isoyear' from date '2006-01-01');
+select extract('isoyear' from current_timestamp);
+
+select date_trunc('year', timestamp '2020-07-30 20:38:40');
+select date_trunc('year', current_timestamp);
+
+--날짜에서 월 추출
+select date_part('month', timestamp '2020-07-30 20:38:40');
+select date_part('month', current_timestamp);
+
+select extract('month' from timestamp '2020-07-30 20:38:40');
+select extract('month' from interval '2 years 3 months');
+select extract('month' from interval '2 years 13 months');
+
+select date_trunc('month', timestamp '2020-07-30 20:38:40');
+
+--날짜에서 일 추출
+select date_part('day', timestamp '2020-07-30 20:38:40');
+
+select date_trunc('day', timestamp '2020-07-30 20:38:40');
+
+--시간에서 시 추출
+select date_part('hour', timestamp '2013-07-30 20:38:40');
+select date_part('hour', interval '4 hours 3 minutes');
+
+select date_trunc('hour', timestamp '2020-07-30 20:38:40');
+
+--시간에서 분 추출
+select date_part('minute', timestamp '2020-07-30 20:38:40');
+
+select date_trunc('minute', timestamp '2020-07-30 20:38:40');
+
+--시간에서 초 추출
+select date_part('second', timestamp '2013-07-30 20:38:40');
+
+select extract('second' from time '17:12:28.5');
+
+select date_trunc('second', timestamp '2013-07-30 20:38:40');
+
+
+--실습2
+--주수 구하기
+select EMP_NAME, to_char((current_timestamp - ENT_DATE),'W') WEEKS
+from cslee.TB_EMP
+where ORG_CD = '10';
+
+--to char
+select emp_name, ent_date,
+	to_char(ent_date,'yyyy') 입사년,
+	to_char(ent_date,'MM') 입사월,
+	to_char(ent_date,'DD') 입사일
+from cslee.tb_emp;
+
+--extract
+select emp_name, ent_date, 
+extract('year' from ent_date) 입사년,
+extract('month' from ent_date) 입사월,
+extract('day' from ent_date) 입사일
+from cslee.tb_emp;
+
+--변환함수(to_date, to_number, to_timestamp, cast)
+select to_date('05 Dec 2000', 'DD Mon YYYY');
+
+select to_number('12,454.8-', '99G999D9S');
+
+select to_timestamp('05 Dec 2000', 'DD Mon YYYY');
+
+select CAST(123.4 AS VARCHAR(10)), 
+	CAST('123.5' AS NUMERIC),
+	CAST(1234.5678 AS DEC(6,2)),
+	CAST(CURRENT_TIMESTAMP AS DATE),
+	TO_CHAR(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS'), 
+	TO_CHAR(1234.56, 'L9,999.99'), 
+	TO_NUMBER('$12,345', '$99,999'), 
+	TO_DATE('2014/03/01 21:30:18','YYYY/MM/DD HH24:MI:SS'), 
+	TO_TIMESTAMP('2014/03/01 21:30:18','YYYY/MM/DD HH24:MI:SS');
+
+--case
+select emp_name,
+	case when salary > 50000000
+	then salary
+	else 50000000
+	end as revised_salary
+from cslee.tb_emp;
+
+select org_name,
+	case org_name
+		when '영업1팀' then '지사'
+		when '영업2팀' then '지사'
+		when '영업3팀' then '지사'
+		when '경영관리팀' then '본사'
+		else '지점'
+	end as AREA
+from cslee.tb_org;
+
+select EMP_NAME, 
+	case when SALARY >= 90000000 then 'HIGH'
+		when SALARY >= 60000000 then 'MID'
+		else 'LOW'
+	end as SALARY_GRADE
+from cslee.TB_EMP;
+
+--중첩 case
+select EMP_NAME, SALARY, 
+	case when SALARY >= 50000000
+			then 20000000
+		else (case when SALARY >= 20000000
+						then 10000000
+					else SALARY * 0.5
+				end)
+	end as BONUS
+from cslee.TB_EMP;
+
+--null
+--coalesce
+select EMP_NAME, position, 
+	coalesce(POSITION,'없음')
+from cslee.TB_EMP;
+
+--case
+select EMP_NAME, POSITION, 
+case when POSITION IS NULL
+then '없음'
+else POSITION
+end as 직책
+from cslee.TB_EMP
+
+--null과 공집합
+select coalesce(salary, 0) SAL 
+from cslee.tb_emp
+where emp_name='김태진';
+
+select MAX(salary) SAL 
+from cslee.tb_emp
+where emp_name='김태진';
+
+select coalesce(MAX(salary), 9999) SAL 
+from cslee.tb_emp
+where emp_name='김태진';
